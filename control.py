758357e7e0fe3395d5a3263881698d98bec651d8
NathanBurtnett <61424244+NathanBurtnett@users.noreply.github.com>
2023-02-02 08:23:15 -0800

    Initial Setup

---
+++ control.py
@@ -0,0 +1,18 @@
+class control:
+    """!
+
+    """
+
+    def __init__(self):
+
+        set proportional gain
+        set initial setpoint
+        ... other parameters
+
+    def run(self):
+
+    def set_setpoint(self):
+
+    def set_Kp(self):
+
+    def print_time(self):
