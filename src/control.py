f93bb8538b877944e661c2a69d370ad48e17eaa9
NathanBurtnett <61424244+NathanBurtnett@users.noreply.github.com>
2023-02-02 08:37:22 -0800

    Update control.py

--- src/control.py
+++ src/control.py
@@ -1,20 +1,26 @@
+import pyb
+
 class control:
     """!
 
     """
 
-    def __init__(self):
+    def __init__(self, Kp, setpoint, initial_output):
         """!
         """
-
-        set proportional gain
-        set initial setpoint
-        set other parameters
+        self.Kp = Kp
+        self.setpoint = setpoint
+        self.output = initial_output
+        self.times = []
+        self.positions = []
 
     def run(self, setpoint, measured_output, motor_actuation):
         """!
         """
-
+        error = setpoint - measured_output
+        motor_actuation = self.Kp * error
+        self.times.append(utime.ticks_ms())
+        self.positions.append(measured_output)
         return motor_actuation
 
     def set_setpoint(self):
