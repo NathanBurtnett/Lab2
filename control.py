2971e4e1335365fe0a73e4688bece5b3a947c47c
NathanBurtnett <61424244+NathanBurtnett@users.noreply.github.com>
2023-02-02 08:25:43 -0800

    Update control.py

--- control.py
+++ control.py
@@ -4,15 +4,29 @@ class control:
     """
 
     def __init__(self):
+        """!
+        """
 
         set proportional gain
         set initial setpoint
         ... other parameters
 
     def run(self):
+        """!
+        """
 
     def set_setpoint(self):
+        """!
+        """
 
     def set_Kp(self):
+        """!
+        """
+
+    def store(self):
+        """!
+        """
 
     def print_time(self):
+        """!
+        """
